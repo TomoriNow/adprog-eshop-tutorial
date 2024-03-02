d8b0ee917a6f6c8b6093ef7d7df36b997b2468f1
TomoriNow <seanarcher1974@gmail.com>
2024-03-02 21:23:50 +0700

    [RED] Add Order model skeleton.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -43,8 +43,8 @@ public class OrderTest {
 
         assertSame(this.products, order.getProducts());
         assertEquals(2, order.getProducts().size());
-        assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductName());
-        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());
+        assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getName());
+        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getName());
 
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b", order.getId());
         assertEquals(1708560000L, order.getOrderTime());
